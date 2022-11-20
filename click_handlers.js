843238b607a6d9875e5d69f8915a5bdc5cd9b5e5
mu2so4 <b4.orangutang@gmail.com>
2022-11-20 11:20:50 +0700

    Add HTTP async request

---
+++ click_handlers.js
@@ -0,0 +1,21 @@
+
+function getPlacesOnClick() {
+    const place = document.getElementById('input_place').value
+
+    const http = new XMLHttpRequest()
+    http.onreadystatechange = function() {
+        if (http.readyState === 4 && http.status === 200) {
+            //const response = document.getElementById('response_area')
+            //response.value = http.response
+            alert(http.response)
+        }
+    }
+    const url = 'https://graphhopper.com/api/1/geocode?q=' + place +
+        '&key=69f3bd9d-95bf-4292-a4eb-159ac244774a'
+    http.open("GET", url)
+    http.send()
+
+    //const response = document.getElementById('response_area')
+    //response.value = http.response
+
+}
--- main.js
+++ main.js
@@ -39,9 +39,5 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-function onClick() {
-  alert('Hello world!')
-}
-
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
